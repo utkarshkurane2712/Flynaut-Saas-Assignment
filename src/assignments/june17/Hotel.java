b390c4377af6dd0eda28de36ed7ba9ee48d068f4
Utkarsh Kurane <160477276+utkarshkurane2712@users.noreply.github.com>
2025-06-18 13:39:52 +0530

    New classes added

---
+++ src/assignments/june17/Hotel.java
@@ -0,0 +1,12 @@
+package assignments.june17;
+
+public class Hotel {
+
+    void bookRoom(String roomType){
+        System.out.println("The room "+roomType+" is booked");
+    }
+
+    void booRoom(String roomType, int days){
+        System.out.println("The room "+roomType+" is booked for "+days+" days");
+    }
+}
---
+++ src/assignments/june17/UseHotel.java
@@ -0,0 +1,9 @@
+package assignments.june17;
+
+public class UseHotel {
+    public static void main(String[] args) {
+        Hotel h = new Hotel();
+        h.bookRoom("Deluxe");
+        h.booRoom("Deluxe",7);
+    }
+}
